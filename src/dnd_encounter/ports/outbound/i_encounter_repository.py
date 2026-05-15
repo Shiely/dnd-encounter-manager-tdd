6f4e2e45fc24bb1d3ef958392c42ee3321e67169
jayshilo <jpshiely@yahoo.com>
2026-05-14 20:13:44 -0700

    Phase B: Add IEncounterRepository port

---
+++ src/dnd_encounter/ports/outbound/i_encounter_repository.py
@@ -0,0 +1,7 @@
+# ports/outbound/i_encounter_repository.py
+from typing import Protocol
+from ...domain.entities.encounter import Encounter
+
+class IEncounterRepository(Protocol):
+    def load(self) -> Encounter | None: ...
+    def save(self, encounter: Encounter) -> None: ...
