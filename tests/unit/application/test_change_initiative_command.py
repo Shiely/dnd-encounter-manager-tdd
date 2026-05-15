bee71e4ee98aa385d531fe3d512035f1bd262e19
jayshilo <jpshiely@yahoo.com>
2026-05-14 20:24:01 -0700

    Phase D Final Step: Add failing test for ChangeInitiativeCommand (red)

---
+++ tests/unit/application/test_change_initiative_command.py
@@ -0,0 +1,28 @@
+# tests/unit/application/test_change_initiative_command.py
+
+from src.dnd_encounter.application.commands.change_initiative_command import ChangeInitiativeCommand
+from src.dnd_encounter.domain.entities.encounter import Encounter
+from src.dnd_encounter.domain.entities.encounter_entity import EncounterEntity
+
+
+def test_execute_changes_initiative(stub_publisher):
+    encounter = Encounter(encounter_id="test")
+    entity = EncounterEntity("e1", "Goblin #1", "monster", 10, current_hp=10, max_hp=10, monster_id="goblin")
+    encounter.entities.append(entity)
+
+    cmd = ChangeInitiativeCommand(encounter, "e1", 18, stub_publisher)
+    cmd.execute()
+
+    assert entity.initiative == 18
+
+
+def test_undo_restores_old_initiative(stub_publisher):
+    encounter = Encounter(encounter_id="test")
+    entity = EncounterEntity("e1", "Goblin #1", "monster", 10, current_hp=10, max_hp=10, monster_id="goblin")
+    encounter.entities.append(entity)
+
+    cmd = ChangeInitiativeCommand(encounter, "e1", 18, stub_publisher)
+    cmd.execute()
+    cmd.undo()
+
+    assert entity.initiative == 10
