ab0d0c4f93bffc0fafdb2240aad48109a9f69126
jayshilo <jpshiely@yahoo.com>
2026-05-14 20:10:27 -0700

    TDD Phase A Step 2: Add Condition enum (value object, required for condition_rules)

---
+++ src/dnd_encounter/domain/value_objects/condition.py
@@ -0,0 +1,19 @@
+# DOMAIN LAYER: stdlib imports only.
+import enum
+
+class Condition(enum.Enum):
+    BLINDED = "Blinded"
+    CHARMED = "Charmed"
+    DEAFENED = "Deafened"
+    EXHAUSTION = "Exhaustion"
+    FRIGHTENED = "Frightened"
+    GRAPPLED = "Grappled"
+    INCAPACITATED = "Incapacitated"
+    INVISIBLE = "Invisible"
+    PARALYZED = "Paralyzed"
+    PETRIFIED = "Petrified"
+    POISONED = "Poisoned"
+    PRONE = "Prone"
+    RESTRAINED = "Restrained"
+    STUNNED = "Stunned"
+    UNCONSCIOUS = "Unconscious"
