ad28736263fda62938c832b6fdec99eab82dfdec
jayshilo <jpshiely@yahoo.com>
2026-05-14 20:32:22 -0700

    Phase E: Complete EncounterService with all major methods

--- src/dnd_encounter/application/services/encounter_service.py
+++ src/dnd_encounter/application/services/encounter_service.py
@@ -9,9 +9,14 @@ from src.dnd_encounter.ports.outbound.i_undo_stack import IUndoStack
 from src.dnd_encounter.ports.outbound.i_dice_roller import IDiceRoller
 from src.dnd_encounter.ports.outbound.i_event_publisher import IEventPublisher
 from src.dnd_encounter.application.dto.encounter_dto import EncounterStateDTO, EntityRowDTO, MonsterSummaryDTO
-from src.dnd_encounter.application.commands.add_entity_command import AddEntityCommand
-from src.dnd_encounter.application.commands.edit_hp_command import EditHpCommand
-# ... other commands will be imported as needed
+from src.dnd_encounter.application.commands import (
+    AddEntityCommand,
+    EditHpCommand,
+    ToggleConditionCommand,
+    RemoveEntityCommand,
+    RenameEntityCommand,
+    ChangeInitiativeCommand,
+)
 
 @dataclass
 class EncounterService:
@@ -58,6 +63,72 @@ class EncounterService:
         self.encounter_repo.save(self.encounter)
         return self._to_dto()
 
-    # Other methods (edit_hp, toggle_condition, advance_turn, undo, etc.) will be added in next commits
+    def edit_hp(self, instance_id: str, new_hp: int) -> EncounterStateDTO:
+        cmd = EditHpCommand(
+            encounter=self.encounter,
+            instance_id=instance_id,
+            new_hp=new_hp,
+            publisher=self.publisher,
+        )
+        cmd.execute()
+        self.undo_stack.push(cmd)
+        self.encounter_repo.save(self.encounter)
+        return self._to_dto()
+
+    def toggle_condition(self, instance_id: str, condition_name: str) -> EncounterStateDTO:
+        from src.dnd_encounter.domain.value_objects.condition import Condition
+        condition = Condition(condition_name)
+        cmd = ToggleConditionCommand(
+            encounter=self.encounter,
+            instance_id=instance_id,
+            condition=condition,
+            publisher=self.publisher,
+        )
+        cmd.execute()
+        self.undo_stack.push(cmd)
+        self.encounter_repo.save(self.encounter)
+        return self._to_dto()
+
+    def remove_entity(self, instance_id: str) -> EncounterStateDTO:
+        cmd = RemoveEntityCommand(
+            encounter=self.encounter,
+            instance_id=instance_id,
+            publisher=self.publisher,
+        )
+        cmd.execute()
+        self.undo_stack.push(cmd)
+        self.encounter_repo.save(self.encounter)
+        return self._to_dto()
+
+    def advance_turn(self) -> EncounterStateDTO:
+        if self.encounter.entities:
+            self.encounter.current_turn_index = (self.encounter.current_turn_index + 1) % len(self.encounter.entities)
+            if self.encounter.current_turn_index == 0:
+                self.encounter.round_number += 1
+        self.encounter_repo.save(self.encounter)
+        return self._to_dto()
+
+    def undo(self) -> EncounterStateDTO:
+        if self.undo_stack.is_empty():
+            return self._to_dto()
+        cmd = self.undo_stack.pop()
+        if cmd:
+            cmd.undo()
+        self.encounter_repo.save(self.encounter)
+        return self._to_dto()
+
     def get_state(self) -> EncounterStateDTO:
         return self._to_dto()
+
+    def get_monster_definition(self, monster_id: str) -> MonsterSummaryDTO | None:
+        definition = self.monster_repo.get(monster_id)
+        if not definition:
+            return None
+        return MonsterSummaryDTO(
+            monster_id=definition.id,
+            name=definition.name,
+            cr=str(definition.challenge_rating.value),
+            hp=definition.hit_points,
+            ac=definition.armor_class,
+            source=definition.source,
+        )
